d4d3eb90f31330cb12d51e98c71e272977ab5583
JesseScott <jessecolinscott@gmail.com>
2014-10-31 23:54:49 -0700

    only working with action bar

---
+++ replace.js
@@ -0,0 +1,21 @@
+
+console.log('REPLACE.JS');
+
+jQuery.fn.textWalk = function( fn ) {
+    this.contents().each( jwalk );
+
+    function jwalk() {
+        var nn = this.nodeName.toLowerCase();
+        if( nn === '#text') {
+            fn.call( this );
+        } else if( this.nodeType === 1 && this.childNodes && this.childNodes[0] && nn !== 'script' && nn !== 'textarea' ) {
+            $(this).contents().each( jwalk );
+        }
+    }
+    return this;
+};
+
+$('body').textWalk(function() {
+    this.data = this.data.replace('Jian', 'Nobody');
+    this.data = this.data.replace(/[Rr]eplace\s[Ss]ome\s[Tt]ext/g, 'with other text');
+});
